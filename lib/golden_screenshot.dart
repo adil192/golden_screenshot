4583fdebda86cc0141e7ee5f3faa873182ab4e83
Adil Hanney <adilhanney@disroot.org>
2024-08-09 15:48:32 +0100

    chore: add doc comment to base file

--- lib/golden_screenshot.dart
+++ lib/golden_screenshot.dart
@@ -1,3 +1,6 @@
+/// Utilities to automate screenshot generation using Flutter's golden tests.
+library;
+
 export 'src/screenshot_app.dart';
 export 'src/screenshot_device.dart';
 export 'src/screenshot_frame.dart';
