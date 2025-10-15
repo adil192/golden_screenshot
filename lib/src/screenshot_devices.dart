ee14013e1fd32f2c358d58ce7e0682a118c13f11
Adil Hanney <adilhanney@disroot.org>
2025-10-15 18:40:31 +0100

    feat: add low resolution devices

--- lib/src/screenshot_devices.dart
+++ lib/src/screenshot_devices.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:golden_screenshot/golden_screenshot.dart';
 
 /// The default set of [ScreenshotDevice]s.
+///
+/// See [GoldenSmallDevices] for the equivalent devices at a lower resolution
+/// to speed up tests.
 enum GoldenScreenshotDevices {
   /// A desktop/laptop running Linux.
   ///
@@ -72,4 +75,68 @@ enum GoldenScreenshotDevices {
 class ScreenshotDevices {
   static List<ScreenshotDevice> get values =>
       GoldenScreenshotDevices.values.map((e) => e.device).toList();
+}
+
+/// Devices with a small resolution for fast testing.
+///
+/// See [GoldenScreenshotDevices] for the full resolution equivalents.
+enum GoldenSmallDevices {
+  /// A desktop/laptop running Linux.
+  ///
+  /// The size fits Flathub's guidelines for screenshots
+  /// (https://docs.flathub.org/docs/for-app-authors/metainfo-guidelines/quality-guidelines/#reasonable-window-size)
+  /// while also being a 16:9 aspect ratio for Google Play.
+  flathub(ScreenshotDevice(
+    platform: TargetPlatform.linux,
+    resolution: Size(1920 * sf, 1080 * sf),
+    pixelRatio: 1.5 * sf,
+    goldenSubFolder: 'tenInchScreenshots/',
+    frameBuilder: ScreenshotFrame.noFrame,
+  )),
+
+  /// An Android phone based on the Pixel 6 Pro.
+  android(ScreenshotDevice(
+    platform: TargetPlatform.android,
+    resolution: Size(1440 * sf, 3120 * sf),
+    pixelRatio: 10 / 3 * sf,
+    goldenSubFolder: 'phoneScreenshots/',
+    frameBuilder: ScreenshotFrame.android,
+  )),
+
+  /// A MacBook Pro (15-inch, 2019)
+  /// with a "scaled resolution" of 1440x900.
+  macbook(ScreenshotDevice(
+    platform: TargetPlatform.macOS,
+    resolution: Size(2880 * sf, 1800 * sf),
+    pixelRatio: 2 * sf,
+    goldenSubFolder: 'macbookScreenshots/',
+    frameBuilder: ScreenshotFrame.noFrame,
+  )),
+
+  /// iPhone 16 Pro Max,
+  /// labelled on App Store Connect as iPhone 6.9" Display.
+  iphone(ScreenshotDevice(
+    platform: TargetPlatform.iOS,
+    resolution: Size(1320 * sf, 2868 * sf),
+    pixelRatio: 3 * sf,
+    goldenSubFolder: 'iphoneScreenshots/',
+    frameBuilder: ScreenshotFrame.iphone,
+  )),
+
+  /// iPad Pro 13" (M4),
+  /// labelled on App Store Connect as iPad 13" Display.
+  ipad(ScreenshotDevice(
+    platform: TargetPlatform.iOS,
+    resolution: Size(2064 * sf, 2752 * sf),
+    pixelRatio: 2 * sf,
+    goldenSubFolder: 'ipadScreenshots/',
+    frameBuilder: ScreenshotFrame.ipad,
+  ));
+
+  const GoldenSmallDevices(this.device);
+  final ScreenshotDevice device;
+
+  /// The scale factor applied to each device's resolution.
+  @visibleForTesting
+  static const sf = 0.2;
 }
