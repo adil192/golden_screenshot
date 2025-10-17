517e2e7c1e03573674c0275f8843330baac0244d
Adil Hanney <adilhanney@disroot.org>
2025-10-17 14:06:07 +0100

    test: deprecated getters
    To get my coverage percentage up

---
+++ example/test/other_tests/deprecated_devices_test.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_screenshot/golden_screenshot.dart';
+
+void main() {
+  test('Deprecated devices', () {
+    expect(GoldenScreenshotDevices.newerIphone, GoldenScreenshotDevices.iphone);
+    expect(GoldenScreenshotDevices.olderIphone, GoldenScreenshotDevices.iphone);
+    expect(GoldenScreenshotDevices.newerIpad, GoldenScreenshotDevices.ipad);
+    expect(GoldenScreenshotDevices.olderIpad, GoldenScreenshotDevices.ipad);
+    expect(ScreenshotDevices.values,
+        GoldenScreenshotDevices.values.map((e) => e.device).toList());
+  });
+}
