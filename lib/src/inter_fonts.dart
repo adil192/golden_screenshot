e7730361d0af81730e3433095ee5cdb86cb49fc1
Adil Hanney <adilhanney@disroot.org>
2026-01-31 03:05:10 +0000

    feat: augment Inter with more language support

--- lib/src/inter_fonts.dart
+++ lib/src/inter_fonts.dart
@@ -2,23 +2,16 @@ import 'package:flutter/services.dart';
 
 abstract class InterFonts {
   static final assetFutures = const [
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-Black.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-BlackItalic.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-Bold.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-BoldItalic.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-ExtraBold.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-ExtraBoldItalic.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-ExtraLight.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-ExtraLightItalic.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-Italic.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-Light.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-LightItalic.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-Medium.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-MediumItalic.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-Regular.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-SemiBold.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-SemiBoldItalic.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-Thin.ttf',
-    'packages/golden_screenshot/assets/Inter/Inter_24pt-ThinItalic.ttf',
+    // We're now using Inter augmented with Noto Sans for more language support.
+    // See https://github.com/adil192/inter-noto-hybrid
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-Regular.ttf',
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-Black.ttf',
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-Bold.ttf',
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-ExtraBold.ttf',
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-ExtraLight.ttf',
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-Light.ttf',
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-Medium.ttf',
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-SemiBold.ttf',
+    'packages/golden_screenshot/assets/InterNotoSansHybrid/InterNotoSansHybrid-Thin.ttf',
   ].map(rootBundle.load).toList(growable: false);
 }
