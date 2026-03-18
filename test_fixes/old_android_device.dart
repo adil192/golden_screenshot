c4354640d0f75042cbdbda9b574972c76130c5ab
Adil Hanney <adilhanney@disroot.org>
2026-03-18 22:57:41 +0000

    test: fix test_fixes by enabling deprecated_member_use_from_same_package lint

--- test_fixes/old_android_device.dart
+++ test_fixes/old_android_device.dart
@@ -1,8 +1,8 @@
 import 'package:golden_screenshot/golden_screenshot.dart';
 
 void main() {
-  final device = GoldenScreenshotDevices.android;
-  final smallDevice = GoldenSmallDevices.android;
-  final frameBuilder = ScreenshotFrame.android;
-  final frameImage = ScreenshotFrame.androidTopBarImage;
+  final _ = GoldenScreenshotDevices.android;
+  final _ = GoldenSmallDevices.android;
+  final _ = ScreenshotFrame.android;
+  final _ = ScreenshotFrame.androidTopBarImage;
 }
